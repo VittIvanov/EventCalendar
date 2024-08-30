13e237585c5db2e4ffe957b558955453fede2a0d
VittIvanov <ivanov.vitaly.wdm@mail.ru>
2024-08-30 19:02:27 +0600

    update mock-files

--- src/mocks/mockParticipants.ts
+++ src/mocks/mockParticipants.ts
@@ -9,91 +9,91 @@ export const mockParticipants: {
       id: 1,
       name: "Егор",
       avatar: "Speaker",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "egor@example.com",
+      password: "qwe123456#"
     },
     {
       id: 2,
       name: "Анна",
       avatar: "Attendee",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "anna@example.com",
+      password: "qwe123456#"
     },
     {
       id: 3,
       name: "Рита",
       avatar: "Organizer",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "rita@example.com",
+      password: "qwe123456#"
     },
     {
       id: 4,
       name: "Феликс",
       avatar: "Organizer",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "felix@example.com",
+      password: "qwe123456#"
     },
     {
       id: 5,
       name: "Эмилия",
       avatar: "Organizer",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "emilya@example.com",
+      password: "qwe123456#"
     },
     {
       id: 6,
       name: "Никита",
       avatar: "Organizer",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "nikita@example.com",
+      password: "qwe123456#"
     },
     {
       id: 7,
       name: "Роман",
       avatar: "Organizer",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "roman@example.com",
+      password: "qwe123456#"
     },
     {
       id: 8,
       name: "Оля",
       avatar: "Organizer",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "olya@example.com",
+      password: "qwe123456#"
     },
     {
       id: 9,
       name: "Антон",
       avatar: "Organizer",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "anton@example.com",
+      password: "qwe123456#"
     },
     {
       id: 10,
       name: "Екатерина",
       avatar: "Organizer",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "ekaterina@example.com",
+      password: "qwe123456#"
     },
     {
       id: 11,
       name: "Денис",
       avatar: "Organizer",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "denis@example.com",
+      password: "qwe123456#"
     },
     {
       id: 12,
       name: "Элика",
       avatar: "Organizer",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "elica@example.com",
+      password: "qwe123456#"
     },
     {
       id: 13,
       name: "Илья",
       avatar: "Organizer",
-      e_mail: "jvS0M@example.com",
-      password: "123456"
+      e_mail: "ilya@example.com",
+      password: "qwe123456#"
     },
   ];
